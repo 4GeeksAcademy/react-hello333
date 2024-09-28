4a6959f841b970709c180d819d64820667616781
Jyebenes <131881275+Jyebenes@users.noreply.github.com>
2024-09-28 04:06:17 +0000

    algunos cambios

---
+++ Simple-Counter-main/Simple-counter/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import SecondsCounter from './componentes/SecondsCounter';
+
+
+const App = () => {
+  return (
+
+   <SecondsCounter seconds={10} />
+   
+  );
+}
+
+  
+  
+
+
+export default App
